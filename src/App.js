145b35e31c82efe9cf99142de9a33dabded42d7d
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-25 13:30:18 -0500

    add header with Bottsrao componenets

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Navbar from "./Components/Navbar/Header";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+      </div>
+    );
+  }
+}
