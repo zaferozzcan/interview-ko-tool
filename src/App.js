fb2d77f0e0f08a102aa0981337115e83b841371b
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-11-25 22:17:21 -0500

    add landing and banner component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import Landing from "./Components/Landing/Landing";
 import Navbar from "./Components/Navbar/Header";
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user_id: "",
+    };
+  }
+
   render() {
     return (
-      <div>
-        <Navbar />
+      <div className="container">
+        <Route exact path="/" component={(Landing, Navbar)}>
+          <Navbar user_id={this.state.user_id} />
+          <Landing />
+        </Route>
       </div>
     );
   }
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Banner() {
+  return (
+    <div className="banner-container">
+      <div className="banner-inner">
+        <h1>Time For a Sharp Interview</h1>
+      </div>
+    </div>
+  );
+}
