1fa851a1461ebc6ccae153d044b68abf519f1a05
ZAFER OZCAN <zaferozzcan@gmail.com>
2020-12-01 23:34:57 -0500

    add subject holder

--- src/components/Subject/Subject.jsx
+++ src/components/Subject/Subject.jsx
@@ -4,8 +4,11 @@ import Tabs from "./tabs/Tabs";
 export default function Subject() {
   return (
     <div className="subject-container">
-      <div className="subject-container-box">
-        <Tabs />
+      <div className="suject-container-inner">
+        <div className="subject-container-box">
+          <Tabs />
+          <div className="subject-holder"></div>
+        </div>
       </div>
     </div>
   );
